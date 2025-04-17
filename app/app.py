e628a96f538633898e6b7acbf5c4cc47bf2633a1
Khoa <khoakg1707@gmail.com>
2025-04-16 22:26:52 -0700

    remove ssl

--- app/app.py
+++ app/app.py
@@ -69,4 +69,4 @@ def config_devices():
     return {"success": True}
 
 if __name__ == '__main__':
-    app.run(ssl_context=('../secrets/forever.crt', '../secrets/forever.key'), host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- smartnest-webui/src/configs.js
+++ smartnest-webui/src/configs.js
@@ -1,1 +1,1 @@
-export const API_URL = "https://192.168.1.10:5000";
+export const API_URL = "https://smartnest.stellarnest.xyz:5000";
