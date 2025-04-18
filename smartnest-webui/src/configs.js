24985d69fd3feecbe09ee95a9a7da8526162d828
Khoa <khoakg1707@gmail.com>
2025-04-18 01:23:02 -0700

    change endpoint to api.stellarnest.xyz

--- smartnest-webui/src/configs.js
+++ smartnest-webui/src/configs.js
@@ -1,1 +1,1 @@
-export const API_URL = (process.env.NODE_ENV === "production") ? "https://smartnest.stellarnest.xyz": "http://localhost:5000";
+export const API_URL = (process.env.NODE_ENV === "production") ? "https://api.stellarnest.xyz": "http://localhost:5000";
