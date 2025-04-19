aadf9ac08224a9dc283f34928d83aeb64b145a16
Khoa <khoakg1707@gmail.com>
2025-04-19 16:50:39 -0700

    fix frontend

--- smartnest-webui/src/App.js
+++ smartnest-webui/src/App.js
@@ -7,7 +7,7 @@ import { API_URL } from './configs'
 import { getCookieValue } from './utils';
 function App() {
   const [deviceList, setDeviceList] = useState([]);
-  const fetchDevices = async () => {
+  const fetchDevices = () => {
 
     // try {        
     //   const cfToken = getCookieValue('CF_Authorization');
