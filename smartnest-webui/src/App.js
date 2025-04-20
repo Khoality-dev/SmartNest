5e097a77d68519dc390bf51166e06042452e3e9b
Khoa <khoakg1707@gmail.com>
2025-04-19 17:44:07 -0700

    fix

--- smartnest-webui/src/App.js
+++ smartnest-webui/src/App.js
@@ -7,27 +7,9 @@ import { API_URL } from './configs'
 import { getCookieValue } from './utils';
 function App() {
   const [deviceList, setDeviceList] = useState([]);
-  const [eventSource, setEventSource] = useState(null);
-  const fetchDevices = () => {
 
-    // try {        
-    //   const cfToken = getCookieValue('CF_Authorization');
-    //   const response = await axios.get(API_URL + '/list-devices', {
-    //     headers: {
-    //       'Authorization': `Bearer ${cfToken}`
-    //     }
-    //   });
-    //   console.log('Response:', response.data.devices);
-    //   response.data.devices.mediaStatus = {}
-    //   let devices = response.data.devices.map((device) => {
-    //     return { ...device, mediaStatus: { file_name: device.file_name, duration: device.duration, position: device.position, paused: device.status == "Playing" ? false : true, looping: device.looping, volume: device.volume } }
-    //   })
-    //   setDeviceList(devices);
-    // } catch (error) {
-    //   console.error(error);
-    // }
-    
-    setEventSource(new EventSource(API_URL + '/list-devices'));
+  useEffect(() => {
+    eventSource = new EventSource(API_URL + '/list-devices');
     eventSource.onmessage = function(event) {
       const devices = JSON.parse(event.data);
       console.log('Devices:', devices);
@@ -44,16 +26,9 @@ function App() {
       }));
       setDeviceList(transformed);
     };
-  }
-
-
-  useEffect(() => {
-    fetchDevices();
-    const intervalId = setInterval(() => {
-      console.log('Fetching devices...');
-      fetchDevices();
-    }, 60000);
-    return () => clearInterval(intervalId);
+    return () => {
+      eventSource.close();
+    }
   }, []);
 
   return (
